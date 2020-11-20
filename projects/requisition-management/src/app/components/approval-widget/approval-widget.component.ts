75374db78c39cbe2167d6ed91e5a98ec2e2602fd
MKless <mkless@intershop.de>
2020-11-20 15:13:15 +0100

    fix: adjust observable handling in widgets

--- projects/requisition-management/src/app/components/approval-widget/approval-widget.component.ts
+++ projects/requisition-management/src/app/components/approval-widget/approval-widget.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { filter, map, startWith } from 'rxjs/operators';
+import { map, startWith } from 'rxjs/operators';
 
 import { PriceItemHelper } from 'ish-core/models/price-item/price-item.helper';
 import { Price, PriceHelper } from 'ish-core/models/price/price.model';
 import { GenerateLazyComponent } from 'ish-core/utils/module-loader/generate-lazy-component.decorator';
 
 import { RequisitionManagementFacade } from '../../facades/requisition-management.facade';
+import { Requisition } from '../../models/requisition/requisition.model';
 
 @Component({
   selector: 'ish-approval-widget',
@@ -23,19 +24,24 @@ export class ApprovalWidgetComponent implements OnInit {
   constructor(private requisitionFacade: RequisitionManagementFacade) {}
 
   ngOnInit() {
-    const pendingApprovals$ = this.requisitionFacade
-      .approverPendingRequisitions$()
-      .pipe(filter(reqs => reqs.length > 0));
+    const pendingApprovals$ = this.requisitionFacade.approverPendingRequisitions$();
 
     this.numPendingApprovals$ = pendingApprovals$.pipe(
-      startWith([]),
+      startWith([] as Requisition[]),
       map(reqs => reqs.length)
     );
     this.totalAmountApprovals$ = pendingApprovals$.pipe(
-      map(reqs => reqs.map(req => PriceItemHelper.selectType(req.totals?.total, 'gross'))),
-      map(prices =>
-        prices?.reduce((curr: Price, acc: Price) => PriceHelper.sum(curr, acc), PriceHelper.empty(prices[0].currency))
-      )
+      map(reqs => reqs?.map(req => PriceItemHelper.selectType(req.totals?.total, 'gross'))),
+      map(prices => {
+        if (prices.length > 0) {
+          return prices?.reduce(
+            (curr: Price, acc: Price) => PriceHelper.sum(curr, acc),
+            PriceHelper.empty(prices[0].currency ?? undefined)
+          );
+        } else {
+          return PriceHelper.empty();
+        }
+      })
     );
 
     this.requisitionsLoading$ = this.requisitionFacade.requisitionsLoading$;
--- projects/requisition-management/src/app/components/requisition-widget/requisition-widget.component.ts
+++ projects/requisition-management/src/app/components/requisition-widget/requisition-widget.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { filter, map, startWith } from 'rxjs/operators';
+import { map, startWith } from 'rxjs/operators';
 
 import { PriceItemHelper } from 'ish-core/models/price-item/price-item.helper';
 import { Price, PriceHelper } from 'ish-core/models/price/price.model';
 import { GenerateLazyComponent } from 'ish-core/utils/module-loader/generate-lazy-component.decorator';
 
 import { RequisitionManagementFacade } from '../../facades/requisition-management.facade';
+import { Requisition } from '../../models/requisition/requisition.model';
 
 @Component({
   selector: 'ish-requisition-widget',
@@ -23,19 +24,24 @@ export class RequisitionWidgetComponent implements OnInit {
   constructor(private requisitionFacade: RequisitionManagementFacade) {}
 
   ngOnInit() {
-    const pendingRequisitions$ = this.requisitionFacade
-      .buyerPendingRequisitions$()
-      .pipe(filter(reqs => reqs.length > 0));
+    const pendingRequisitions$ = this.requisitionFacade.buyerPendingRequisitions$();
 
     this.numPendingRequisitions$ = pendingRequisitions$.pipe(
-      startWith([]),
-      map(reqs => reqs?.length)
+      startWith([] as Requisition[]),
+      map(reqs => reqs.length)
     );
     this.totalAmountRequisitons$ = pendingRequisitions$.pipe(
       map(reqs => reqs?.map(req => PriceItemHelper.selectType(req.totals?.total, 'gross'))),
-      map(prices =>
-        prices?.reduce((curr: Price, acc: Price) => PriceHelper.sum(curr, acc), PriceHelper.empty(prices[0].currency))
-      )
+      map(prices => {
+        if (prices.length > 0) {
+          return prices?.reduce(
+            (curr: Price, acc: Price) => PriceHelper.sum(curr, acc),
+            PriceHelper.empty(prices[0].currency ?? undefined)
+          );
+        } else {
+          return PriceHelper.empty();
+        }
+      })
     );
 
     this.requisitionsLoading$ = this.requisitionFacade.requisitionsLoading$;
