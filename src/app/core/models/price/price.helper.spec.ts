4881b331f776c24d13492b98efeeb8eda70f41ba
MKless <mkless@intershop.de>
2020-11-20 15:40:34 +0100

    fix: correct price test naming

--- src/app/core/models/price/price.helper.spec.ts
+++ src/app/core/models/price/price.helper.spec.ts
@@ -128,7 +128,7 @@ describe('Price Helper', () => {
   });
 
   describe('empty', () => {
-    it('should return an empty price with the right currency', () => {
+    it('should always return an empty price with the right currency', () => {
       const emptyPrice = PriceHelper.empty('USD');
       expect(emptyPrice.currency).toEqual('USD');
       expect(emptyPrice.value).toEqual(0);
